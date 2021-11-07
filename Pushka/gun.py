9bd2e6c4ab4fe06b3323a205debbf9a28e6ebb87
hasoart <artash.gyolecyan@gmail.com>
2021-11-07 20:55:05 +0300

    Added moving gun, retry button

--- Pushka/gun.py
+++ Pushka/gun.py
@@ -17,6 +17,7 @@ CYAN = 0x00FFCC
 BLACK = (0, 0, 0)
 WHITE = 0xFFFFFF
 GREY = 0x7D7D7D
+LIGHT_GREY = 0xCDCDCD
 ORANGE = 0xFF7200
 GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
@@ -125,6 +126,9 @@ class Gun:
         self.f2_on = 0
         self.an = 1
         self.color = GREY
+        self.x = 20
+        self.y = 450
+        self.speed = 2
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -137,9 +141,9 @@ class Gun:
         """
         global balls, bullet
         bullet += 1
-        new_ball = Ball(self.screen)
+        new_ball = Ball(self.screen, x=self.x, y=self.y)
         new_ball.r += 5
-        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
+        self.an = math.atan2((event.pos[1]-self.y), (event.pos[0]-self.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
         balls.append(new_ball)
@@ -149,7 +153,7 @@ class Gun:
     def targetting(self, event):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan2(event.pos[1]-450, event.pos[0]-20)
+            self.an = math.atan2(event.pos[1]-self.y, event.pos[0]-self.x)
         if self.f2_on:
             self.color = ORANGE
         else:
@@ -158,14 +162,13 @@ class Gun:
     def draw(self):
         gun_w = 7
         gun_l = self.f2_power
-        x_gun = 20
-        y_gun = 450
+
         sin_an = math.sin(self.an)
         cos_an = math.cos(self.an)
-        coords = [(x_gun + gun_w * 0.5 * sin_an, y_gun - gun_w * 0.5 * cos_an),
-                  (x_gun + gun_w * 0.5 * sin_an + gun_l * cos_an, y_gun - gun_w * 0.5 * cos_an + gun_l * sin_an),
-                  (x_gun - gun_w * 0.5 * sin_an + gun_l * cos_an, y_gun + gun_w * 0.5 * cos_an + gun_l * sin_an),
-                  (x_gun - gun_w * 0.5 * sin_an, y_gun + gun_w * 0.5 * cos_an)]
+        coords = [(self.x + gun_w * 0.5 * sin_an, self.y - gun_w * 0.5 * cos_an),
+                  (self.x + gun_w * 0.5 * sin_an + gun_l * cos_an, self.y - gun_w * 0.5 * cos_an + gun_l * sin_an),
+                  (self.x - gun_w * 0.5 * sin_an + gun_l * cos_an, self.y + gun_w * 0.5 * cos_an + gun_l * sin_an),
+                  (self.x - gun_w * 0.5 * sin_an, self.y + gun_w * 0.5 * cos_an)]
 
         pygame.draw.polygon(screen, self.color, coords)
 
@@ -177,6 +180,31 @@ class Gun:
         else:
             self.color = GREY
 
+    def move(self, direction):
+        """
+        Двигает пушку.
+        returns - None
+
+        arguments:
+        direction(str) - направление движения. Передается ГЕЙмерским методом(wasd)
+        """
+
+        if direction == 'w':
+            self.y -= self.speed
+            if self.y < 20:
+                self.y = 20
+        if direction == 's':
+            self.y += self.speed
+            if self.y > 550:
+                self.y = 550
+        if direction == 'd':
+            self.x += self.speed
+            if self.x > 780:
+                self.x = 780
+        if direction == 'a':
+            self.x -= self.speed
+            if self.x < 20:
+                self.x = 20
 
 class Target:
     def __init__(self):
@@ -221,8 +249,8 @@ class TargetHorizontal(Target):
     def move(self, dt=1):
         self.x += self.v * dt
 
-        if not -self.r < self.x < 800 + self.r:
-            self.x = (1 - self.r) if (self.x >= 800 + self.r) else (799 + self.r)
+        if not -self.r < self.x < WIDTH + self.r:
+            self.x = (1 - self.r) if (self.x >= WIDTH + self.r) else (WIDTH + self.r - 1)
 
 
 class TargetVertical(Target):
@@ -241,161 +269,185 @@ class TargetVertical(Target):
     def move(self, dt=1):
         self.y += self.v * dt
 
-        if not -self.r < self.y < 600 + self.r:
-            self.y = (1 - self.r) if (self.y >= 600 + self.r) else (599 + self.r)
+        if not -self.r < self.y < HEIGHT + self.r:
+            self.y = (1 - self.r) if (self.y >= HEIGHT + self.r) else (HEIGHT + self.r - 1)
 
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-bullet = 0
-balls = []
 
-clock = pygame.time.Clock()
-gun = Gun(screen)
-targets = [TargetHorizontal() for _ in range(3)] +\
-          [TargetVertical() for _ in range(3)]
-score = 0
-
-finished = False
-start_time = time()
-
-while targets:
-    screen.fill(WHITE)
-
-    gun.draw()
-
-    for target in targets:
-        target.move()
-        target.draw()
-
-    for b in balls:
-        b.draw()
-
-    score_text = small_font.render(f'Score {score}', True, BLACK)
-    screen.blit(score_text, (20, 20))
-    bullet_text = small_font.render(f'Bullets {bullet}', True, BLACK)
-    screen.blit(bullet_text, (20, 40))
-    time_text = small_font.render(f'Time {time() - start_time:.1f}', True, BLACK)
-    screen.blit(time_text, (20, 60))
-
-    pygame.display.update()
-
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_program()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            gun.fire2_start(event)
-        elif event.type == pygame.MOUSEBUTTONUP:
-            gun.fire2_end(event)
-        elif event.type == pygame.MOUSEMOTION:
-            gun.targetting(event)
-
-    c = 0
-    for b in balls:
-        b.move()
-        for i, target in enumerate(targets):
-            if b.hittest(target) and target.live:
-                target.live = 0
-                score += target.hit()
-                targets.pop(i)
-                balls.pop(c)
-                break
-        else:
-            if not b.is_moving:
-                balls.pop(c)
-            else:
-                c += 1
-
-    gun.power_up()
-
-final_time = round(time() - start_time, 1)
-final_score = int(10000 * score / (final_time * bullet))
-
-player_name = ""
-name_entered = False
-
-while not name_entered:
-    screen.fill(WHITE)
-
-    row = []
-    row += [big_font.render(f'Final score is {final_score}', True, BLACK)]
-    row += [big_font.render(f'Enter your name', True, BLACK)]
-    row += [big_font.render(player_name, True, BLACK)]
-
-    y_offset = 150
-    for i in range(3):
-        width, height = row[i].get_size()
-        x_offset = (WIDTH - width) / 2
-        screen.blit(row[i], (x_offset, y_offset))
-
-        y_offset += height
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_program()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                name_entered = True
-            elif event.key == pygame.K_BACKSPACE:
-                player_name = player_name[:-1]
-            else:
-                player_name += event.unicode
-
-    pygame.display.update()
-    clock.tick(FPS)
-
-try:
-    with open('leaderboard.json', 'r') as f:
-        leaderboard = json.load(f)
-    leaderboard['scores'].append({'name': player_name, 'score': final_score})
-    leaderboard['scores'] = sorted(leaderboard['scores'], key=lambda x: x.get('score'), reverse=True)
-    with open('leaderboard.json', 'w') as f:
-        json.dump(leaderboard, f, ensure_ascii=False, indent=4)
-except:
-    leaderboard = {'scores': [{'name': player_name, 'score': final_score}]}
-    with open('leaderboard.json', 'w') as f:
-        json.dump(leaderboard, f, ensure_ascii=False, indent=4)
-
-leaderboard_surface = pygame.Surface((800, 600))
-leaderboard_surface.fill(WHITE)
-title_text = big_font.render('Leaderboard', True, BLACK)
-
-width, height = title_text.get_size()
-
-x_offset = (WIDTH - width) / 2
-y_offset = 50
-
-leaderboard_surface.blit(title_text, (x_offset, y_offset))
-y_offset += 100
-rows = [None for _ in range(10)]
-leaderboard_length = 10 if len(leaderboard['scores']) > 10 else len(leaderboard['scores'])
-for i in range(leaderboard_length):
-    name, score = leaderboard['scores'][i]['name'], leaderboard['scores'][i]['score']
-    row = pygame.Surface((300, 30))
-    row.fill(WHITE)
-    name_surface = small_font.render(f'{name}', True, BLACK)
-    score_surface = small_font.render(f'{score}', True, BLACK)
-    row.blit(name_surface, (0, 0))
-    row.blit(score_surface, (200, 0))
-
-    rows[i] = row
-
-for row in rows:
-    if row is not None:
-        width, height = row.get_size()
-        x_offset = (WIDTH - width) / 2
-        leaderboard_surface.blit(row, (x_offset, y_offset))
-        y_offset += height
-    else:
-        break
 
 while True:
-    screen.blit(leaderboard_surface, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_program()
+    bullet = 0
+    balls = []
 
-    pygame.display.update()
-    clock.tick(FPS)
+    clock = pygame.time.Clock()
+    gun = Gun(screen)
+    targets = [TargetHorizontal() for _ in range(3)] + \
+              [TargetVertical() for _ in range(3)]
+    score = 0
+
+    finished = False
+    start_time = time()
+
+    while targets:
+        screen.fill(WHITE)
+
+        gun.draw()
+
+        for target in targets:
+            target.move()
+            target.draw()
+
+        for b in balls:
+            b.draw()
+
+        score_text = small_font.render(f'Score {score}', True, BLACK)
+        screen.blit(score_text, (20, 20))
+        bullet_text = small_font.render(f'Bullets {bullet}', True, BLACK)
+        screen.blit(bullet_text, (20, 40))
+        time_text = small_font.render(f'Time {time() - start_time:.1f}', True, BLACK)
+        screen.blit(time_text, (20, 60))
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            gun.move('w')
+        if keys[pygame.K_s]:
+            gun.move('s')
+        if keys[pygame.K_d]:
+            gun.move('d')
+        if keys[pygame.K_a]:
+            gun.move('a')
+
+        pygame.display.update()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_program()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                gun.fire2_start(event)
+            elif event.type == pygame.MOUSEBUTTONUP and gun.f2_on:
+                gun.fire2_end(event)
+            elif event.type == pygame.MOUSEMOTION:
+                gun.targetting(event)
+
+        c = 0
+        for b in balls:
+            b.move()
+            for i, target in enumerate(targets):
+                if b.hittest(target) and target.live:
+                    target.live = 0
+                    score += target.hit()
+                    targets.pop(i)
+                    balls.pop(c)
+                    break
+            else:
+                if not b.is_moving:
+                    balls.pop(c)
+                else:
+                    c += 1
+
+        gun.power_up()
+
+    final_time = round(time() - start_time, 1)
+    final_score = int(10000 * score / (final_time * bullet))
+
+    player_name = ""
+    name_entered = False
+
+    while not name_entered:
+        screen.fill(WHITE)
+
+        row = []
+        row += [big_font.render(f'Final score is {final_score}', True, BLACK)]
+        row += [big_font.render(f'Enter your name', True, BLACK)]
+        row += [big_font.render(player_name, True, BLACK)]
+
+        y_offset = 150
+        for i in range(3):
+            width, height = row[i].get_size()
+            x_offset = (WIDTH - width) / 2
+            screen.blit(row[i], (x_offset, y_offset))
+
+            y_offset += height
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_program()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    name_entered = True
+                elif event.key == pygame.K_BACKSPACE:
+                    player_name = player_name[:-1]
+                else:
+                    player_name += event.unicode
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    try:
+        with open('leaderboard.json', 'r') as f:
+            leaderboard = json.load(f)
+        leaderboard['scores'].append({'name': player_name, 'score': final_score})
+        leaderboard['scores'] = sorted(leaderboard['scores'], key=lambda x: x.get('score'), reverse=True)
+        with open('leaderboard.json', 'w') as f:
+            json.dump(leaderboard, f, ensure_ascii=False, indent=4)
+    except:
+        leaderboard = {'scores': [{'name': player_name, 'score': final_score}]}
+        with open('leaderboard.json', 'w') as f:
+            json.dump(leaderboard, f, ensure_ascii=False, indent=4)
+
+    leaderboard_surface = pygame.Surface((800, 600))
+    leaderboard_surface.fill(WHITE)
+    title_text = big_font.render('Leaderboard', True, BLACK)
+
+    width, height = title_text.get_size()
+
+    x_offset = (WIDTH - width) / 2
+    y_offset = 50
+
+    leaderboard_surface.blit(title_text, (x_offset, y_offset))
+    y_offset += 100
+    rows = [None for _ in range(10)]
+    leaderboard_length = 10 if len(leaderboard['scores']) > 10 else len(leaderboard['scores'])
+    for i in range(leaderboard_length):
+        name, score = leaderboard['scores'][i]['name'], leaderboard['scores'][i]['score']
+        row = pygame.Surface((300, 30))
+        bg_color = WHITE if i % 2 else LIGHT_GREY
+        row.fill(bg_color)
+        name_surface = small_font.render(f'{name}', True, BLACK)
+        score_surface = small_font.render(f'{score}', True, BLACK)
+        row.blit(name_surface, (3, 3))
+        row.blit(score_surface, (203, 3))
+
+        pygame.draw.rect(row, BLACK, (0, 0, 300, 30), width=3)
+
+        rows[i] = row
+
+
+    for row in rows:
+        if row is not None:
+            width, height = row.get_size()
+            x_offset = (WIDTH - width) / 2
+            leaderboard_surface.blit(row, (x_offset, y_offset))
+            y_offset += height
+        else:
+            break
+
+    retry = False
+
+    while not retry:
+        screen.blit(leaderboard_surface, (0, 0))
+        pygame.draw.rect(screen, GREEN, (600, 400, 200, 200))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_program()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x,  y = event.pos
+                if x >= 600 and y >= 400:
+                    retry = True
+
+        pygame.display.update()
+        clock.tick(FPS)
 
